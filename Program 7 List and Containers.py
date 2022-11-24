f955a6fa04c09435270acfd4d91c2fa45c24b593
Dylankuneman04 <dylankuneman04@gmail.com>
2022-11-24 13:44:27 -0500

    initial commit

---
+++ Program 7 List and Containers.py
@@ -0,0 +1,15 @@
+def get_input():
+    a = 0
+    eight_inputs = []
+    while a != 8:
+        get_inputs = input("Please enter eight numbers: ")
+        a += 1
+        eight_inputs.append(get_inputs)
+    return eight_inputs, get_inputs
+
+
+def four_space_display
+
+
+
+four_space_display()
